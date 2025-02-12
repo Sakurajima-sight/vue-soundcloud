ddf81fe07509617b7f86d4c582d7e1d4a0797a1f
rick <617289776@qq.com>
2025-02-13 03:04:47 +0800

    added element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,33 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <el-row justify="center">
+    <el-button type="primary">{{ msg }}</el-button>
+  </el-row>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Button Text',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+  h1, h2 {
+    font-weight: normal;
   }
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import {
+    ElRow,
+    ElButton,
+    ElLoading,
+    ElMessageBox,
+    ElNotification,
+    ElMessage,
+} from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './router'
 
-createApp(App).mount('#app')
+// 创建 Vue 应用实例
+const app = createApp(App)
+
+// 使用 Element Plus 组件
+app.use(ElButton)
+app.use(ElRow)
+app.use(router)
+app.use(ElLoading.directive)
+// 设置全局属性
+app.config.globalProperties.$loading = ElLoading.service;
+app.config.globalProperties.$msgbox = ElMessageBox;
+app.config.globalProperties.$alert = ElMessageBox.alert;
+app.config.globalProperties.$confirm = ElMessageBox.confirm;
+app.config.globalProperties.$prompt = ElMessageBox.prompt;
+app.config.globalProperties.$notify = ElNotification;
+app.config.globalProperties.$message = ElMessage;
+app.mount('#app')
